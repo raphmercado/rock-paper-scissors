48b984064968c1441ebf56836a52c4a6549eea20
Jose Raphael Mercado <98337482+raphmercado@users.noreply.github.com>
2023-05-07 23:25:19 +0800

    Add functionalities to rock paper scissors buttons
    - Commented out function playGame for now

--- index.js
+++ index.js
@@ -2,60 +2,70 @@ function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3) + 1;
   switch (computerChoice) {
     case 1:
-      return "Rock";
+      return "rock";
     case 2:
-      return "Paper";
+      return "paper";
     case 3:
-      return "Scissors";
+      return "scissors";
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  const choices = playerSelection.concat(computerSelection).toLowerCase();
-  let isWin = true;
+  const choices = playerSelection.concat(computerSelection);
+  console.log(`Computer: ${computerSelection}\nPlayer: ${playerSelection}`);
+  //let isWin = true;
   switch (choices) {
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
-      return isWin;
+      //return isWin;
+      return console.log("Win!");
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
-      return isWin = false;
+      //return isWin = false;
+      return console.log("Lose!");
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
-      return isWin = "Draw";
+      //return isWin = "Draw";
+      return console.log("Draw!");
   }
 }
 
-function playGame() {
+function getPlayerChoice(e) {
+  const playerChoice = e.srcElement.id;
+  playRound(playerChoice, getComputerChoice());
+}
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => button.addEventListener('click', getPlayerChoice));
+
+/*function playGame() {
   let round = 5;
   let playerSelection;
   let computerSelection;
   let playerScore = 0;
   let computerScore = 0;
   for (let i = 1; i <= round; i++) {
-    playerSelection = prompt(`Round ${i}\nEnter your play (Rock | Paper | Scissors): `);
-    computerSelection = getComputerChoice();
-    let isRoundWin = playRound(playerSelection, computerSelection);
-    if (isRoundWin === true) {
-      playerScore += 1;
-      alert(`You Win! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} beats ${computerSelection}\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
-    } else if (isRoundWin === false) {
-      computerScore += 1;
-      alert(`You Lose! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} loses to ${computerSelection}\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
-    } else if (isRoundWin === "Draw") {
-      alert(`Draw!\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
-    }
-  } if (playerScore > computerScore) {
-    alert("Game Over! You Win!");
-  } else if (playerScore < computerScore) {
-    alert("Game Over! You Lose. Better luck next time!");
-  } else if (playerScore === computerScore) {
-    alert("Game Over! It's a Draw!");
-  }
-
+playerSelection = prompt(`Round ${i}\nEnter your play (Rock | Paper | Scissors): `);
+computerSelection = getComputerChoice();
+let isRoundWin = playRound(playerSelection, computerSelection);
+if (isRoundWin === true) {
+  playerScore += 1;
+  alert(`You Win! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} beats ${computerSelection}\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
+} else if (isRoundWin === false) {
+  computerScore += 1;
+  alert(`You Lose! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} loses to ${computerSelection}\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
+} else if (isRoundWin === "Draw") {
+  alert(`Draw!\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
 }
-
-playGame();
+} if (playerScore > computerScore) {
+alert("Game Over! You Win!");
+} else if (playerScore < computerScore) {
+alert("Game Over! You Lose. Better luck next time!");
+} else if (playerScore === computerScore) {
+alert("Game Over! It's a Draw!");
+}
+}
+playGame();*/
