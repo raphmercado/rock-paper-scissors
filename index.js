2c1c7f059d71f2dc32e8f09a93a8fd102bb7a66c
Jose Raphael Mercado <98337482+raphmercado@users.noreply.github.com>
2023-05-07 19:18:48 +0800

    Add function playGame, modify function playRound

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  const computerChoice = Math.floor(Math.random() * 3) + 1;
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
   switch (computerChoice) {
     case 1:
       return "Rock";
@@ -12,21 +12,50 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   const choices = playerSelection.concat(computerSelection).toLowerCase();
+  let isWin = true;
   switch (choices) {
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
-      return `You Win! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`;
+      return isWin;
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
-      return `You Lose! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} loses to ${computerSelection}`;
-    default:
-      return "Draw!";
+      return isWin = false;
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      return isWin = "Draw";
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function playGame() {
+  let round = 5;
+  let playerSelection;
+  let computerSelection;
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 1; i <= round; i++) {
+    playerSelection = prompt(`Round ${i}\nEnter your play (Rock | Paper | Scissors): `);
+    computerSelection = getComputerChoice();
+    let isRoundWin = playRound(playerSelection, computerSelection);
+    if (isRoundWin === true) {
+      playerScore += 1;
+      alert(`You Win! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} beats ${computerSelection}\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
+    } else if (isRoundWin === false) {
+      computerScore += 1;
+      alert(`You Lose! ${playerSelection[0].toUpperCase()}${playerSelection.slice(1).toLowerCase()} loses to ${computerSelection}\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
+    } else if (isRoundWin === "Draw") {
+      alert(`Draw!\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}\n`);
+    }
+  } if (playerScore > computerScore) {
+    alert("Game Over! You Win!");
+  } else if (playerScore < computerScore) {
+    alert("Game Over! You Lose. Better luck next time!");
+  } else if (playerScore === computerScore) {
+    alert("Game Over! It's a Draw!");
+  }
 
-console.log(playRound(playerSelection, computerSelection));
+}
+
+playGame();
